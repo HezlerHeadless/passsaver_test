2253f56376ebf971bdb460c6cefacb7da2a26c9f
Vladimir <bratka.vov@gmail.com>
2022-12-11 10:19:38 +0200

    init progect

---
+++ src/main/java/ua/passsaver/passsaver_test/PasssaverTestApplication.java
@@ -0,0 +1,13 @@
+package ua.passsaver.passsaver_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasssaverTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasssaverTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/passsaver/passsaver_test/PasssaverTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.passsaver.passsaver_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasssaverTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
